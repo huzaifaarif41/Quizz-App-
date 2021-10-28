9be60441fa13d0bf825238140beacf1aceba77f4
unknown <aflaharif2005@gmail.com>
2021-10-29 01:26:46 +0500

    Assignment_Quiz App

---
+++ src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+  {
+    prompt: "What is my name?",
+    optionA: "Maaz",
+    optionB: "Ali",
+    optionC: "Ahsan",
+    optionD: "Huzaifa",
+    asnwer: "optionD",
+  },
+  {
+    prompt: "Which programming language I love the most?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "React JS",
+    optionD: "Java",
+    asnwer: "optionC",
+  },
+  {
+    prompt: "What I am learning now?",
+    optionA: "Java",
+    optionB: "Angular",
+    optionC: "C#",
+    optionD: "React JS",
+    asnwer: "optionD",
+  },
+];
